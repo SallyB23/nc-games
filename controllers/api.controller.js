425d11100d569d635492d4f012dadd8618340964
SallyB23 <sally.bentley.1612@googlemail.com>
2022-07-07 14:40:21 +0100

    update api controller

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,10 +1,8 @@
 const { fetchEndpoints } = require("../models/api.models")
 
-exports.getEndpoints = (req, res, next) => {
+exports.getEndpoints = (req, res) => {
     fetchEndpoints().then((endpoints) => {
+        const parsedEndpoints = JSON.parse(endpoints)
         res.status(200).send({ endpoints })
-    })
-    .catch(err => {
-        console.log(err, "<<err")
     })
 }
